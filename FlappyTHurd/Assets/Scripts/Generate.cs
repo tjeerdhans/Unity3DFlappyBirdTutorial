d6ab1f33ca0d210e3b17e39ad52178d37dc9469d
Tjeerd Hans Terpstra <tjeerdhans@outlook.com>
2016-05-09 20:01:29 +0200

    Initial commit.

---
+++ FlappyTHurd/Assets/Scripts/Generate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    public GameObject rocks;
+    private int score = -2;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        if (score > -1)
+        {
+            GUILayout.Label(" Score: " + score.ToString());
+        }
+    }
+
+    void CreateObstacle()
+    {
+        Instantiate(rocks);
+        score++;
+    }
+}
